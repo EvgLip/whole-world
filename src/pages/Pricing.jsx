37e15e8303d316cf6fe521837b0d41de569ff792
evgLip <evggenlip@gmail.com>
2025-01-07 10:17:41 +0300

    косметические правки

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -8,7 +8,7 @@ export default function Product ()
       <section>
         <div>
           <h2>
-            Всего 100 &#x20bd;/месяц.
+            Всего 1000 &#x20bd; за бессрочное пользование.
           </h2>
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel
@@ -16,7 +16,7 @@ export default function Product ()
             voluptatem iste.
           </p>
         </div>
-        <img src="img-2.jpg" alt="overview of a large city with skyscrapers" />
+        <img src="img-2.jpg" alt="обзор большого города с небоскребами" />
       </section>
     </main>
   );
